65eb8fac3d5ba4973661d7b1f202179a4dd82aff
aiwareai <piotrmacai0@gmail.com>
2024-05-27 00:08:15 +0200

    added homepage sections

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,15 @@ import Hero from "@/components/Hero";
 import Brands from "@/components/Brands";
 import Feature from "@/components/Features";
 import About from "@/components/About";
+import AboutWeb from "@/components/AboutWeb";
+import AboutAi from "@/components/AboutAi";
+import AboutMarketing from "@/components/AboutMarketing";
 import FeaturesTab from "@/components/FeaturesTab";
+import WebFeaturesTab from "@/components/FeaturesTabWeb";
+import AiFeaturesTab from "@/components/FeaturesTabAi";
+import MarketingFeaturesTab from "@/components/FeaturesTabMarketing";
+import HeaderAi from "@/components/HeaderAi";
+import HeaderWeb from "@/components/HeaderWeb";
 import FunFact from "@/components/FunFact";
 import GenAiHome from "@/components/GenAiHome";
 import Integration from "@/components/Integration";
@@ -40,16 +48,34 @@ export default function Home() {
       <Hero />
       <Feature /> 
        <About />           
-      <FeaturesTab /> 
+      {/* <FeaturesTab />  */}
        <HomePortfolio/>    
-      <GenAiHome/>             
+                 
     
       {/* <PortfolioCta/> */}
-      <FunFact />
+  
       {/* <Integration /> */}
       {/* <FAQ /> */}
       {/* <Testimonial /> */}
-       <Pricing />      
+      <div id="headerWeb">
+      {/* <HeaderWeb/> */}
+      <AboutWeb/>
+      <WebFeaturesTab/>
+      </div>
+      <div id="headerAi">
+      {/* <HeaderAi/>      */}
+      <AboutAi/>
+      <AiFeaturesTab/>
+      </div> 
+      <div id="headerMarketing">
+      <AboutMarketing/>
+      <MarketingFeaturesTab/>
+      </div> 
+      <div id="headerGenAi">
+      <GenAiHome/> 
+      </div>  
+       <Pricing />   
+       <FunFact /> 
        <CTA />
       <Contact /> 
       <Brands />   
